959e772400c90bef2fce692762e62e339b21aa47
ebuka-hub <albertebuka69@gmail.com>
2025-03-29 07:53:28 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+list = ['apple', 'mango', 'Banana','Guvana','kiwi']
+
+print("lenght of list:", len(list))
+print("first element:", list[0])
+print("last Element:", list[-1])
+
+list.append('papaya')
+print("updated list")
